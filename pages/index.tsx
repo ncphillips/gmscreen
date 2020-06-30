102c5ede54229a112390a2c4b2cd688bafef2768
Nolan Phillips <ncphillips.19@gmail.com>
2020-06-30 16:19:42 -0300

    encounter list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,29 @@
-import * as firebase from 'firebase/app';
-import { DisplayEncounter } from '@components/encounter';
 import { Layout } from '@components/layout';
 import { useAuthRequiredRedirect } from '@auth';
+import { useEncounterCollection } from '@encounters';
 
 export default function Home() {
   useAuthRequiredRedirect();
 
   return (
     <Layout>
-      <DisplayEncounter />;
+      <EncounterList />
     </Layout>
+  );
+}
+
+function EncounterList() {
+  const encounters = useEncounterCollection();
+
+  if (encounters.loading) {
+    return <h3>Loading Encounters</h3>;
+  }
+
+  return (
+    <ul>
+      {encounters.data.map((encounter) => (
+        <li key={encounter.id}>{encounter.name}</li>
+      ))}
+    </ul>
   );
 }
