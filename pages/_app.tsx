4a3af09ca02f4bd9257832f81b0344d20b2c3032
Nolan Phillips <ncphillips.19@gmail.com>
2020-07-02 20:03:11 -0300

    remove auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,7 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import { useAuth } from '@auth';
-import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }) {
-  const auth = useAuth();
-
-  useEffect(() => {
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
-  }, []);
-
   return <Component {...pageProps} />;
 }
